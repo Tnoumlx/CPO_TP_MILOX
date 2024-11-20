7ba412b3f28ee3f08ec8668ef268dc7d364f449e
Antoine Milox <antoine.milox@epfedu.fr>
2024-11-20 16:34:11 +0100

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_MILOX/src/tp2_bieres_milox/TP2_Bieres_MILOX.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_milox;
+
+/**
+ *
+ * @author miloxantoine
+ */
+public class TP2_Bieres_MILOX {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    /* EXERCICE 1 */
+    
+    BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0 ,"Dubuisson") ;
+
+    
+    uneBiere.lireEtiquette();
+    
+    
+    BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe") ;
+    
+    autreBiere.lireEtiquette();
+    
+    BouteilleBiere BiereTrois = new BouteilleBiere("Zibul", 6.5, "Domaine de Lorkil") ;
+    
+    BiereTrois.lireEtiquette();
+    
+    BouteilleBiere BiereQuatre = new BouteilleBiere("Jurzol", 6.4, "Brasserie des bandits") ;
+    
+    BiereQuatre.lireEtiquette();
+    
+    BouteilleBiere BiereCinq = new BouteilleBiere("Rekop", 6.3, "Chaudron de Ragnar") ;
+    
+    BiereCinq.lireEtiquette();
+    
+    BiereCinq.Decapsuler();
+    BiereCinq.Decapsuler();
+    
+    
+    autreBiere.Decapsuler();
+    
+    
+    
+    System.out.println(uneBiere) ;
+    System.out.println(autreBiere) ;
+    System.out.println(BiereCinq) ;
+    System.out.println(BiereQuatre) ;
+
+    }
+    
+
+    
+    
+
+    
+}
